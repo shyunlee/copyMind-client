88a10c8dd8d0bb00edd01e460889fef137779b8f
baekseungyong <pighorse61@gmail.com>
2020-12-27 21:45:41 +0900

    [Client] feature/#1,#2 Login or Sign CSS for test

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,26 +30,6 @@ export default class Login extends React.Component {
     render(){
         return (
             <div className="view">
-                <article>
-                        <button className="candidate1">Hover me !</button>
-                        <a href="#" className="candidate2 blue">Button</a>
-
-                        <a href="#" className="btn candidate3 animated-button thar-one">Sign up</a>
-                        <a href="#" className="btn candidate3 animated-button thar-two">Login</a>
-                        <a href="#" className="btn candidate3 animated-button thar-three">Register</a>
-                        <a href="#" className="btn candidate3 animated-button thar-four">Learn more</a>
-
-                        <button className="button_submit">Submit</button>
-                        
-                        <div className="button button1">button1</div>
-                        <div className="button button2">button2</div>
-                        <div className="button button3">button3</div>
-                        <div className="button button4">button4</div>
-                        <div className="button button5">button5</div>
-                        <div className="button button6background"><div className="button button6">button6</div></div>
-                        
-
-                </article>
                 <div id="loginInfo">
                     
                     <div id="loginInfo_border">
@@ -66,8 +46,6 @@ export default class Login extends React.Component {
 
 
                         <div className="buttons">
-                            {/* <div className="__Login__"> */}
-                                
                             <div id="LoginButton1" 
                             onClick={()=>{this.props.loginHandler(this.state)}}>
                                 <span>Login</span>
@@ -77,17 +55,7 @@ export default class Login extends React.Component {
                             onClick={()=>{this.props.loginHandler(this.state)}}>
                                 LOG IN
                             </div>
-                                {/* <input id="Login1" type="button" value="Login" 
-                                onClick={
-                                    ()=>{this.props.loginHandler(this.state)}
-                                }
-                                />
-                                <input id="Login2" type="button" value="Login" 
-                                onClick={
-                                    ()=>{this.props.loginHandler(this.state)}
-                                }
-                                /> */}
-                            {/* </div> */}
+
                             <div id="__SignUp__">
                                 <input type="submit" id="SignUp3" value="SignUp" 
                                 onClick={
@@ -106,24 +74,6 @@ export default class Login extends React.Component {
                         <input id="OAuth-Github" type="button" value="LOGIN WITH GITHUB" />
                     </div>
                 </div>
-
-                <aside>
-                    <div className="button button7">button7</div>
-                    <div className="button button8">button8</div>
-                    <div className="button button9">button9</div>
-                    <div className="button button10">button10</div>
-                    <div className="button button11">button11</div>
-                    <div className="button button12">button12</div>
-                    <div className="button button13">button13</div>
-                    <div className="button button14">button14</div>
-                    <a className="button button15" href="#"><span>Hover</span><span>Button15</span></a>
-                    <div className="button button16">button16</div>
-                    <div className="button button17">button17</div>
-                    <div className="button button18">button18</div>
-                    <div className="button button19">button19</div>
-                    <div className="button button20">button20</div>
-                    <div className="button button21">button21</div>
-                </aside>
 
             </div>
         )
