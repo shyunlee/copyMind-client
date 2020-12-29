c46be6770113123794c0ca7c7a60a9fc0047f47f
SEAN LEE <seanlee@MacBook-Pro.local>
2020-12-29 01:12:46 -0600

    [Client] Merge for completed issue #7, #5, #8, #9, #6, #4, #2

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,4 @@
 export { default as _Main } from './_Main';
-export { default as _Login } from './_Login';
 export { default as _ListCopy } from './_ListCopy';
 export { default as _ViewCopy } from './_ViewCopy';
 export { default as _Nav } from './_Nav';
